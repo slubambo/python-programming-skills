7a6ea5b4a3d57640c15145baeeebbe2c9da95f61
slubambo <slubambo57@gmail.com>
2023-02-10 19:47:45 +0300

    Friday Class

---
+++ Recamp/friday.py
@@ -0,0 +1,15 @@
+day = "Sunday"
+
+#print("Its a", day, "❤️")
+
+number_of_beers = 10
+
+CRATES = 3
+
+
+#print("Jordan took", number_of_beers, CRATES)
+
+
+students = ['Jane', 'Doe', 'John']
+
+print[-5]
